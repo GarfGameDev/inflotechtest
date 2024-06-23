7ea327456f7199b2df4e6622591427bead139a0f
GarfGameDev <connorgamedev@hotmail.com>
2024-06-23 13:35:50 +0100

    Added code to Program to overwrite default Logging

--- UserManagement.Web/Program.cs
+++ UserManagement.Web/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using UserManagement.Data;
 using Westwind.AspNetCore.Markdown;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(opt =>
